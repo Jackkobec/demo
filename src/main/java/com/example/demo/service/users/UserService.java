16bbbd78080fa7416e394e86f6ebf9832e7485d8
jack <e.kobets@talantarium.com>
2017-06-15 16:12:42 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/service/users/UserService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service.users;
+
+import com.example.demo.domain.user.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getUsers();
+
+    User getUserById(Long id);
+}
