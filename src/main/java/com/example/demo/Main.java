24aea1075116f6a9f5dd273d3c0b4574bbd3947e
jack <e.kobets@finik.pro>
2018-05-17 16:38:58 +0300

    Stable v2.

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -47,7 +47,7 @@ public class Main {
 //                );
 //    }
 
-    public static LinkedHashMap<String, Object> serializeNeededFields(final Object forSerialize, final List<String> neededFields) {
+    /*public static Map<String, Object> serializeNeededFields(final Object forSerialize, final List<String> neededFields) {
 
         if(forSerialize == null || neededFields == null || neededFields.isEmpty()) {
             return new LinkedHashMap<>();
@@ -67,13 +67,43 @@ public class Main {
                             try {
                                 return Arrays.stream(forSerializeFields)
                                         .filter(forSerializeField -> forSerializeField.getName().equals(neededField)).findFirst().orElseGet(null).get(forSerialize);
-                            } catch (IllegalAccessException e) {
-                                e.printStackTrace();
+                            } catch (final IllegalAccessException e) {
+                                System.out.println("LOG");
+                                return new LinkedHashMap<>();
                             }
-
-                            return null;
                         },
-                        (u, v) -> { throw new IllegalStateException(String.format("Duplicate key %s", u)); },
+                        (key, value) -> { throw new IllegalStateException(String.format("Duplicate key %s", key)); },
+                        LinkedHashMap::new)
+                );
+    }*/
+
+    public static Map<String, Object> serializeNeededFields(final Object forSerialize, final List<String> neededFields) {
+
+        if(forSerialize == null || neededFields == null || neededFields.isEmpty()) {
+            return new LinkedHashMap<>();
+        }
+
+        final Field[] forSerializeFields = forSerialize.getClass().getDeclaredFields();
+        final Map<String, Object> forSerializeFieldsNamesAndValues = Arrays.stream(forSerializeFields)
+                .peek(field -> field.setAccessible(true))
+                .collect(Collectors.toMap(
+                        Field::getName,
+                        field -> {
+                            try {
+                                return field.get(forSerialize);
+                            } catch (final IllegalAccessException e) {
+                                System.out.println("LOG");
+                                return null;
+                            }
+                        })
+                );
+
+        return neededFields.stream()
+                .filter(forSerializeFieldsNamesAndValues.keySet()::contains)
+                .collect(Collectors.toMap(
+                        neededField -> neededField,
+                        neededField ->  forSerializeFieldsNamesAndValues.getOrDefault(neededField, null),
+                        (key, value) -> { throw new IllegalStateException(String.format("Duplicate key %s", key)); },
                         LinkedHashMap::new)
                 );
     }
